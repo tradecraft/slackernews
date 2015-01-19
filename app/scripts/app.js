fe7435078725fe3c3a94542a8f9c68da9f835844
Marc Lang <tradecraft9@gmail.com>
2015-01-19 18:41:28 -0500

    User auth

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -31,6 +31,24 @@ var app = angular
         templateUrl: 'views/showpost.html',
         controller: 'PostViewCtrl'
       })
+      .when('/register', {
+        templateUrl: 'views/register.html',
+        controller: 'AuthCtrl',
+        resolve: {
+          user: function (Auth) {
+            return Auth.resolveUser();
+          }
+        }
+      })
+      .when('/login', {
+        templateUrl: 'views/login.html',
+        controller: 'AuthCtrl',
+        resolve: {
+          user: function (Auth) {
+            return Auth.resolveUser();
+          }
+        }
+      })
       .otherwise({
         redirectTo: '/'
       });
