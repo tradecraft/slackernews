aff06aea5dcaee8460763b8dcb28b6c66c205514
Marc Lang <tradecraft9@gmail.com>
2015-01-19 17:54:00 -0500

    add navbar

--- app/scripts/controllers/posts.js
+++ app/scripts/controllers/posts.js
@@ -2,13 +2,7 @@
 
 app.controller('PostsCtrl', function ($scope, $location, Post) {
   $scope.posts = Post.all;
-  $scope.post = {url: 'http://'};
-
-  $scope.submitPost = function () {
-    Post.create($scope.post).then(function (ref) {
-      $location.path('/posts/' + ref.name());
-    });
-  };
+  $scope.post = {url: 'http://', title: ''};
 
   $scope.deletePost = function (post) {
     Post.delete(post);
