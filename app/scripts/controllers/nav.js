3089d3ec84be1544de4c1c0d239decef04efa0f9
Marc Lang <tradecraft9@gmail.com>
2015-01-19 18:46:03 -0500

    User auth error message display

---
+++ app/scripts/controllers/nav.js
@@ -0,0 +1,14 @@
+'use strict';
+
+app.controller('NavCtrl', function ($scope, $location, Auth, Post) {
+  $scope.post = {url: 'http://', title: ''};
+  $scope.signedIn = Auth.signedIn;
+  $scope.logout = Auth.logout;
+
+  $scope.submitPost = function () {
+    Post.create($scope.post).then(function (ref) {
+      $location.path('/posts/' + ref.name());
+      $scope.post = {url: 'http://', title: ''};
+    });
+  };
+});
